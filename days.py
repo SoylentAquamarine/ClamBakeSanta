96353761325299f5063f625bd35cd56a0820ff0d
Steve <stephen.pleasants@gmail.com>
2025-05-11 15:55:45 -0400

    Update days.py

--- days.py
+++ days.py
@@ -1,8 +1,11 @@
 themes = {
-    "05-11": "National Eat What You Want Day",
-    "05-11": "Hostess CupCake Day",
-    "05-11": "National Train Day",
-    "05-12": "National Nutty Fudge Day",
-    "05-11": "Birthday Blac Chyna",
-    # Add more dates and themes here
+    "05-11": [
+        "National Eat What You Want Day",
+        "Hostess CupCake Day",
+        "National Train Day",
+        "Birthday Blac Chyna"
+    ],
+    "05-12": [
+        "National Nutty Fudge Day"
+    ]
 }
