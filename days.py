98379875ed95ace8fc91010c3598c1b8d505c256
Steve <stephen.pleasants@gmail.com>
2025-05-11 15:53:38 -0400

    Update days.py

--- days.py
+++ days.py
@@ -1,6 +1,8 @@
 themes = {
-    "05-11": "National Shrimp Day",
-    "05-12": "Kevin Costner's Birthday",
-    "05-13": "National Apple Pie Day",
+    "05-11": "National Eat What You Want Day",
+    "05-11": "Hostess CupCake Day",
+    "05-11": "National Train Day",
+    "05-12": "National Nutty Fudge Day",
+    "05-11": "Birthday Blac Chyna",
     # Add more dates and themes here
 }
